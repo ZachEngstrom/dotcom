86d6dd074ada00130d8f615651effd616f2fe795
Zach <zach.webdeveloper@gmail.com>
2014-06-19 13:02:23 -0500

    header tweaks to properly call css

--- themes/zengstrom_v2/home.php
+++ themes/zengstrom_v2/home.php
@@ -2,9 +2,13 @@
 <html>
 	<head>
 		<?php Loader::element('header_required'); ?>
-		<link rel="stylesheet" type="text/css" href="assets/css/home.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="<?php echo $this->getThemePath(); ?>/assets/css/compressed.css.php" media="all" />
+		<?php global $c; if ($c->isEditMode()) { 
+			echo '<link rel="stylesheet" type="text/css" href="' . $this->getThemePath() . '/assets/css/edit.css" media="all" />';
+			}?>
 	</head>
-	<body id="home">
+	<body id="<?php $page = Page::getCurrentPage(); echo str_replace(' ', '_', strtolower($page->getCollectionName())); ?>">
 		<div class="body_wrapper">
 			<header>
 				<div class="center">
